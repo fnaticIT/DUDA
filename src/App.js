4af21d98be8ef4b00476ca3ab990ddbeae0215da
fnaticIT <namanbalai06@gmail.com>
2022-01-22 21:40:44 +0530

    after hosting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from  'react-router-dom'
-import Home from './pages';
-import SigninPage from './pages/signin';
-import Forms from './components/Form/Form';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages";
+import SigninPage from "./pages/signin";
+import Forms from "./components/Form/Form";
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Switch>
-        <Route  path='/' component={Home} exact />
-        <Route  path='/signin' component={SigninPage} exact />
-        <Route  path="/submit" component={Forms} exact />
+        <Route path="/" component={Home} exact />
+        <Route path="/signin" component={SigninPage} exact />
+        <Route path="/submit" component={Forms} exact />
       </Switch>
     </Router>
   );
