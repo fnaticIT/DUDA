fc22728d9ee29153659d0afb27e43ed1d088a41a
fnaticIT <namanbalai06@gmail.com>
2022-01-22 21:44:06 +0530

    after hosting

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages";
 import SigninPage from "./pages/signin";
 import Forms from "./components/Form/Form";
+import { HashRouter } from "react-router-dom";
 function App() {
   return (
-    <Router basename="/">
+    <HashRouter basename="/">
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SigninPage} exact />
         <Route path="/submit" component={Forms} exact />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
 
